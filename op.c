5bb2e73a109f7681c9e22f9465e4c149c782a309
Reini Urban <rurban@cpanel.net>
2015-08-13 15:51:17 +0200

    inline: advance starting pushmark
    to the last before the entersub. they can be nested:
    pushmark pushmark args gv entersub ...

--- op.c
+++ op.c
@@ -8600,7 +8600,9 @@ S_cv_do_inline(pTHX_ OP *o, OP *cvop, CV *cv)
 
     /* handle optional args:
           pushmark args* gv null* entersub body leavesub NULL
-       => pushmark gv rv2av args* push enter? body leave? */
+       => pushmark gv rv2av args* push enter? body leave?
+       TODO: dependent on the body the push args list can be optimized away.
+    */
     arg = S_op_next_nn(o);
     if (S_op_next_nn(arg) != cvop) { /* has args */
         OP *defav;
@@ -15808,6 +15810,7 @@ Perl_rpeep(pTHX_ OP *o)
                 OP* o2 = o;
                 OP* gvop = NULL;
                 /* scan from pushmark to the next entersub call, 4 args with $->$ */
+                while (o->op_next && OP_TYPE_IS(o->op_next, OP_PUSHMARK)) o2 = o = o->op_next;
                 for (; o2 && i<8; o2 = o2->op_next, i++) {
                     OPCODE type = o2->op_type;
                     if (type == OP_GV /*|| type == OP_GVSV */) {
